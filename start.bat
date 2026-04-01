dc50c60b7f72ded542853cc421d54a01d9fd2186
FurkiBu_ <74312970+furkibuu@users.noreply.github.com>
2026-04-01 15:37:40 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,15 @@
-@echo
+@echo off
+title Discord.js v14 Template - Global Bot Starter
+:main
+cls
+echo ------------------------------------------
+echo Starting Discord Bot...
+echo Project: discord.js-v14-template
+echo Powered by Aiko Development
+echo ------------------------------------------
+node index.js
+echo.
+echo [!] Bot has stopped or crashed.
+echo [!] Restarting in 5 seconds...
+timeout /t 5 >nul
+goto main
